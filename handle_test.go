92dcddc4d0ca3d2974bb16d6891f84800cb2c09a
梧桐 <aidecloud@163.com>
2023-09-22 11:12:35 +0800

    feat: 增加新特性 1. 支持替换默认Bind方法

--- handle_test.go
+++ handle_test.go
@@ -1,34 +1,10 @@
 package ginplus
 
 import (
-	"embed"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 )
-
-// Content holds all the SDL file content.
-//
-//go:embed sdl
-var content embed.FS
-
-type Root struct{}
-
-func (r *Root) Ping() string {
-	return "pong"
-}
-
-func TestGraphql(t *testing.T) {
-	instance := New(gin.Default(), WithGraphqlConfig(GraphqlConfig{
-		Enable:     true,
-		HandlePath: "/graphql",
-		ViewPath:   "/graphql",
-		Root:       &Root{},
-		Content:    content,
-	}))
-
-	instance.Run(":8080")
-}
 
 type HandleApi struct {
 }
